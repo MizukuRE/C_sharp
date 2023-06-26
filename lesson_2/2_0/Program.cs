aed176f5a58094fc59e6813efb7b7ac2c858c8b4
MizukuRE <mizuku_re@mail.ru>
2023-06-26 20:50:26 +0300

    classwork 2_0

---
+++ lesson_2/2_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// 0. Напишите программу, которая принимает на вход трёхзначное число
+//    и на выходе показывает последнюю цифру этого числа.
+
+int showLastNum(int num) {
+    Console.WriteLine(num);
+
+    return num % 10;
+}
+
+int rand_num = new Random().Next(100, 1000);
+
+int last_num =showLastNum(rand_num);
+Console.WriteLine(last_num);
