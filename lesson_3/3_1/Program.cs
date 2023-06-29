934c965aaec730315ac91f0f671cd37a146056c7
MizukuRE <mizuku_re@mail.ru>
2023-06-29 21:34:32 +0300

    lesson 3 3_1

---
+++ lesson_3/3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+void PowTable(int n) {
+    if (n == 0) {
+        Console.WriteLine("1");
+        return;
+    }
+
+    int count = 1;
+
+    while (count <= n) {
+        Console.Write($"{Math.Pow(count, 2)}, ");
+
+        count++;
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+PowTable(num);
