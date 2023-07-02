430b4f813e3be412fb72f28254e9f9e1347aef99
MizukuRE <mizuku_re@mail.ru>
2023-07-02 22:08:50 +0300

    Homework 3

---
+++ lesson_3/homework/task_3_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void PowTable(int n)
+{
+    int count = 1;
+
+    if (n == 0)
+    {
+        Console.WriteLine("1 0");
+        return;
+    }
+    else if (n > 0)
+    {
+        while (count <= n)
+        {
+            Console.Write($"{Math.Pow(count, 3)} ");
+
+            count++;
+        }
+    }
+    else 
+    {
+        while (count >= n)
+        {
+            Console.Write($"{Math.Pow(count, 3)} ");
+
+            count--;
+        }
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+PowTable(num);
