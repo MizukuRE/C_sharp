f47de904e2aa6fe681556b310c9bfe5e8e8d8cf2
MizukuRE <mizuku_re@mail.ru>
2023-07-12 20:13:26 +0300

    homework 6

---
+++ lesson_6/homework/task_6_0/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int GreaterThanZeroCount(int[] arr) {
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] > 0)
+            count++;
+
+    return count;
+}
+
+Console.WriteLine("Введите количество чисел");
+int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите числа");
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+    array[i] = int.Parse(Console.ReadLine()!);
+
+int result = GreaterThanZeroCount(array);
+Console.WriteLine("\n" + result);
---
+++ lesson_6/homework/task_6_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+void Intersection(double b1, double k1, double b2, double k2) {
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"({Math.Round(x, 2)}; {Math.Round(y, 2)})");
+}
+
+Console.WriteLine("Введите коэффициент b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент b2");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+Intersection(b1, k1, b2, k2);
