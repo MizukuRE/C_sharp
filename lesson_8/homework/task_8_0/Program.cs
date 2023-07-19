1bf38bc9862baf2b7018526e9fb97c43bba72ad4
MizukuRE <mizuku_re@mail.ru>
2023-07-19 23:06:18 +0300

    homework 8

---
+++ lesson_8/homework/task_8_0/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] RandArr(int rows, int columns, int begin, int end) {
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++) 
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = new Random().Next(begin, end + 1);
+    
+    return arr;
+}
+
+void PrintArr(int[,] array) {
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++)
+            Console.Write($"{array[i, j], 4}");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] SortRows(int[,] array) {
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            for (int k = 0; k < columns - 1; k++)
+                if (array[i, k] < array[i, k + 1])
+                    (array[i, k], array[i, k + 1]) = (array[i, k + 1], array[i, k]);
+
+    return array;
+}
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите начало интервала значений: ");
+int begin = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конец интервала значений: ");
+int end = int.Parse(Console.ReadLine()!);
+
+int[,] array = RandArr(rows, columns, begin, end);
+PrintArr(array);
+Console.WriteLine();
+int[,] sortedArr = SortRows(array);
+PrintArr(sortedArr);
---
+++ lesson_8/homework/task_8_2/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] RandArr(int rows, int columns, int begin, int end) {
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++) 
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = new Random().Next(begin, end + 1);
+    
+    return arr;
+}
+
+void PrintArr(int[,] array) {
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++)
+            Console.Write($"{array[i, j], 4}");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] MatrixComposition(int[,] arrayA, int[,] arrayB) {
+    int rowsA = arrayA.GetLength(0);
+    int columnsA = arrayA.GetLength(1);
+    int columnsB = arrayB.GetLength(1);
+
+    int[,] resultArr = RandArr(rowsA, columnsB, 0, 0);
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < columnsB; j++) {
+            for (int k = 0; k < columnsA; k++) {
+                resultArr[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+
+    return resultArr;
+}
+
+Console.Write("Введите количество строк для первой матрицы: ");
+int rowsA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов для первой матрицы: ");
+int columnsA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите начало интервала значений для первой матрицы: ");
+int beginA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конец интервала значений для первой матрицы: ");
+int endA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество строк для второй матрицы: ");
+int rowsB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов для второй матрицы: ");
+int columnsB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите начало интервала значений для второй матрицы: ");
+int beginB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конец интервала значений для второй матрицы: ");
+int endB = int.Parse(Console.ReadLine()!);
+
+if (columnsA != rowsB) {
+    Console.WriteLine("Нельзя перемножить!");
+        
+    return;
+}
+
+int[,] arrayA = RandArr(rowsA, columnsA, beginA, endA);
+int[,] arrayB = RandArr(rowsB, columnsB, beginB, endB);
+PrintArr(arrayA);
+Console.WriteLine();
+PrintArr(arrayB);
+Console.WriteLine();
+int[,] arrayComp = MatrixComposition(arrayA, arrayB);
+PrintArr(arrayComp);
