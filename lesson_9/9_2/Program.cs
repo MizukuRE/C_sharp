13c1f678a8d2a25b46e3e7f558944b432308c7d3
MizukuRE <mizuku_re@mail.ru>
2023-07-20 21:53:57 +0300

    lesson 9

---
+++ lesson_9/9_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+int SumNum(int num) {
+    if (num / 10 == 0)
+        return num;
+
+    return num % 10 + SumNum(num / 10 );
+}
+
+Console.WriteLine("Введте целое число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = SumNum(num);
+Console.WriteLine(result);
---
+++ lesson_9/9_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+int MyPow(int a, int b) {
+    if (b == 0)
+        return 1;
+
+    return a * MyPow(a, b - 1);
+}
+
+Console.WriteLine("Введте целое число A: ");
+int num_a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введте целое число B: ");
+int num_b = int.Parse(Console.ReadLine()!);
+
+int result = MyPow(num_a, num_b);
+Console.WriteLine(result);
